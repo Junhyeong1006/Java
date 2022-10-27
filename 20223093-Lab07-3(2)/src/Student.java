607a4d36c113bd1ae2280b63deb183ddf2719ad4
Junhyeong1006 <105621923+Junhyeong1006@users.noreply.github.com>
2022-10-27 14:01:41 +0900

    7-3 실습 배열 사용피하기

---
+++ 20223093-Lab07-3(2)/src/Student.java
@@ -0,0 +1,22 @@
+
+public class Student {
+	private String name;
+	private int score;
+
+public Student(String name, int score) {
+	this.name = name;
+	this.score = score;
+}
+//public void setName(String Name) {
+//	name = Name;
+//}	
+public String getName() {
+	return name;	
+}
+//public void setScore(int Score) {
+//	score = Score;
+//}
+public int getScore() {
+	return score;
+}	
+} 
